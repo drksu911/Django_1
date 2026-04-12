71ca2bb49a4cb2721fd5afe6e3258345d62cf8bd
Kseniya Druzhinina <drksu91@mail.ru>
2026-04-12 15:54:18 +0300

    homework_29

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
     path('', views.HomeListView.as_view(), name='home'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    # Кеширование страницы продукта на 15 минут
+    path('product/<int:pk>/', cache_page(60 * 15)(views.ProductDetailView.as_view()), name='product_detail'),
     path('create/', views.ProductCreateView.as_view(), name='product_create'),
     path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
     path('product/<int:pk>/unpublish/', views.UnpublishProductView.as_view(), name='unpublish_product'),
+    path('category/<int:pk>/', views.ProductByCategoryView.as_view(), name='products_by_category'),
 ]
